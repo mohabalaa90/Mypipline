beccc77af24038679f9b53eb3242a36142b21b9c
mohabalaa90 <64390532+mohabalaa90@users.noreply.github.com>
2022-06-11 17:04:56 +0200

    Update myscript.groovy

--- myscript.groovy
+++ myscript.groovy
@@ -5,9 +5,9 @@ def buildJar(){
 def buildImage(){
  echo "building image..............."
  withCredentials([usernamePassword(credentialsId:'dockerHub-Credentials' , passwordVariable:'PASS' , usernameVariable:'USER')]){
- sh "docker build -t mohab98/mohab:MyPl$BUILD_NUMBER ."
- sh "echo $PASS | docker login -u $USER --password-stdin"
- // sh "docker push mohab98/mohab:MyPl$BUILD_NUMBER"
+  sh "docker build -t mohab98/mohab:MyPl$BUILD_NUMBER ."
+  sh "echo $PASS | docker login -u $USER --password-stdin"
+  //sh "docker push mohab98/mohab:MyPl$BUILD_NUMBER"
  }
 }
 return this
