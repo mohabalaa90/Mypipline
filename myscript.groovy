ad017caf6bff5226b07c3f4eddbd4d33ddd479bc
mohabalaa90 <64390532+mohabalaa90@users.noreply.github.com>
2022-06-11 17:25:16 +0200

    Update myscript.groovy

--- myscript.groovy
+++ myscript.groovy
@@ -1,13 +1,20 @@
 def buildJar(){
   echo "building jar file............... "
+  sh 'mvn clean'
   sh 'mvn package'
 }
+
+def clearOldImages(){
+  echo "clearing Old Images ..............."
+  sh "docker images -a | grep "mohab98/mohab*" | awk '{print $3}' | xargs docker rmi -f "
+}
+
 def buildImage(){
   echo "building image..............."
   withCredentials([usernamePassword(credentialsId:'dockerHub-Credentials' , passwordVariable:'PASS' , usernameVariable:'USER')]){
     sh "docker build -t mohab98/mohab:MyPl$BUILD_NUMBER ."
     sh "echo $PASS | docker login -u $USER --password-stdin"
-    sh "docker push mohab98/mohab:MyPl$BUILD_NUMBER"
+    //sh "docker push mohab98/mohab:MyPl$BUILD_NUMBER"
   }
 }
 return this
