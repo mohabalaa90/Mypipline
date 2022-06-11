426bdd89d13ee911c6828ee608984ca8ae0ba7dc
mohabalaa90 <64390532+mohabalaa90@users.noreply.github.com>
2022-06-11 16:51:37 +0200

    Create myscript.groovy

---
+++ myscript.groovy
@@ -0,0 +1,14 @@
+def buildJar(){
+  echo "building jar file............... "
+  sh 'mvn package'
+}
+def buildImage(){
+ echo "building image..............."
+ withCredentials([usernamePassword(credentialsId:'dockerHub-Credentials' , passwordVariable:'PASS' , usernameVariable:'USER')]){
+ sh "docker build -t mohab98/mohab:MyPl$BUILD_NUMBER ."
+ sh "echo $PASS | docker login -u $USER --password-stdin"
+ // sh "docker push mohab98/mohab:MyPl$BUILD_NUMBER"
+}
+ 
+return this
+  
