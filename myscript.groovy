92761bfbf3671008cc4012c66f71e652bda91d40
mohabalaa90 <64390532+mohabalaa90@users.noreply.github.com>
2022-06-12 20:20:37 +0200

    Update myscript.groovy

--- myscript.groovy
+++ myscript.groovy
@@ -1,4 +1,10 @@
-
+def incrementVeresion(){
+  echo "incrementing the version....."
+  sh 'mvn build-helper:parse-version versions:set -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.    \\\${parsedVersion.nextIncrementalVersion} versions:commit'
+  def matcher = readFile('pom.xml')=~ '<version>(.+)</version>'
+  def version = matcher[0][1]
+  env.IMAGE_NAME="$version-$BUILD_NUMBER"
+}
 
 def buildJar(){
   echo "building jar file............... "
@@ -17,12 +23,12 @@ def clearOldImages(){
     fi'''
 }
 
-def buildImage(String imageName){
+def buildImage(String imgN){
   echo "building image..............."
   withCredentials([usernamePassword(credentialsId:'dockerHub-Credentials' , passwordVariable:'PASS' , usernameVariable:'USER')]){
-    sh "docker build -t mohab98/mohab:$imageName$BUILD_NUMBER ."
+    sh "docker build -t mohab98/mohab:$imgN${IMAGE_NAME} ."
     sh "echo $PASS | docker login -u $USER --password-stdin"
-    //sh "docker push mohab98/mohab:imageName$BUILD_NUMBER"
+    //sh "docker push mohab98/mohab:$imgN${IMAGE_NAME}"
   }
 }
 return this
