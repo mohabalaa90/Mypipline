30f5d1092bab1fc7548f6f2d268a7e1d13490bb4
mohabalaa90 <64390532+mohabalaa90@users.noreply.github.com>
2022-06-13 02:25:19 +0200

    Update myscript.groovy

--- myscript.groovy
+++ myscript.groovy
@@ -45,7 +45,7 @@ def commitVersionUpdate(){
     sh "git remote set-url origin https://${USER}:${PASS}@github.com/mohabalaa90/Mypipline.git"
     sh 'git add .'
     sh 'git commit -m "update "'
-    sh "git push https://${USER}:${PASS}@github.com/mohabalaa90/Mypipline.git HEAD:master"
+    sh "git push -u origin HEAD:master"
   }
 }
 
